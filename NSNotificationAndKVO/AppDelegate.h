fb7cac91ffaa7567bc2b852c3985ccdb0f7bae02
Wiljay <wiljay.flores@gmail.com>
2018-08-12 22:31:41 -0400

    first commit

---
+++ NSNotificationAndKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSNotificationAndKVO
+//
+//  Created by Wiljay Flores on 2018-08-12.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
